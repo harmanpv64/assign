bbe27d0552c4cfa4b223c7370daf37237f887610
harmanpv64 <harmanpv64@gmail.com>
2018-06-26 17:40:41 +0530

    ass 1 done

---
+++ assignment1.py
@@ -0,0 +1,43 @@
+# 1 To creat new local repositry
+git
+init
+
+# 2 To tell git who ur our ,configure the author name and email address to be used with commits.
+
+git
+config - -
+global user.name
+"Aarushi048"
+git
+config - -
+global user.email
+aarushi048 @ gmail.com
+
+# 3 To add files
+
+git
+add < filename >
+
+# 4 To list the file we have changed and those we still need to add or commit
+
+git
+status
+
+# 5 To commit
+
+git
+commit - m
+"message"
+
+# 6 To connect to a remote repository
+git
+remote
+add
+origin < url >
+
+# 7 To send message to the master branch of remote repository
+
+git
+push
+
+# 8 git log
