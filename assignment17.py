0958d1f02ffdc49914e664dba5659c74d037690f
harmanpv64 <39954246+harmanpv64@users.noreply.github.com>
2018-07-02 23:09:18 +0530

    Update assignment17.py

--- assignment17.py
+++ assignment17.py
@@ -42,24 +42,3 @@ db=Button(frame,text='Enter',command=cal)
 db.grid(row=4,column=1)
 mainloop()
 
-import tkinter as tk
-def write_slogan():
-    print("HELLO WORLD!!")
-demolabel ="THIS IS A GUI ASSIGNMENT"
-def demo_label(label):
-    label.config(text=str(demolabel))
-
-root = tk.Tk()
-root.title("GUI Asignment 1")
-root.geometry("400x400")
-frame = tk.Frame(root)
-frame.pack()
-button = tk.Button(frame,text="QUIT",fg="red",bg='black',command=quit)
-button.pack(side=tk.LEFT)
-
-slogan = tk.Button(frame,text="Hello",bg='blue',fg='black',command=write_slogan)
-slogan.pack(side=tk.LEFT)
-
-label = tk.Label(root, fg="dark green")
-label.pack()
-demo_label(label)
