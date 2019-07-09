d51811d4d5a20717f4aee41a0f78fe879137067c
harmanpv64 <39954246+harmanpv64@users.noreply.github.com>
2019-07-09 18:39:53 +0530

    Create assign3.py

---
+++ assign3.py
@@ -0,0 +1,38 @@
+#Q1
+l=[]
+a=input("Enter any number:")
+l.append(a)
+a=input("Enter any number:")
+l.append(a)
+print(l)
+
+#Q2
+a=['google','apple','facebook','microsoft','tesla']
+l.append(a)
+print(l)
+
+#Q3
+a=[1,2,3,4,5,3]
+print("count number")
+print(a.count(3))
+
+#Q4
+l=[2,1,4,3,5,7,6]
+l.sort()
+print("sorted")
+print(l)
+
+#Q5
+a=[2,4,6,1,3,5]
+b=[8,10,12,7,9,11]
+l=a+b
+l.sort()
+print(l)
+
+#Q6
+a=[1,2,3,4,5]
+print("stack")
+a.append(6)
+print(a)
+a.pop()
+print(a)
